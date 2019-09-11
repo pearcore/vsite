31d8517ffaa74a2c28d2cf5db9db2dcbed5d1905
pearcore <pearcore@sohu.com>
2019-09-11 15:37:57 +0800

    贼

--- polls/models.py
+++ polls/models.py
@@ -2,16 +2,23 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    class Meta:
+        verbose_name = '提问'
+        verbose_name_plural = '提问' 
+
+    question_text = models.CharField(max_length=200 , verbose_name='所提问题')
+    pub_date = models.DateTimeField('发表时间')
     def __str__(self):
         return self.question_text
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    class Meta:
+        verbose_name = '选项'
+        verbose_name_plural = '选项' 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='所属问题')
+    choice_text = models.CharField(max_length=200, verbose_name='选项内容')
+    votes = models.IntegerField(default=0, verbose_name='投票数')
     def __str__(self):
         return self.choice_text
     def was_published_recently(self):
