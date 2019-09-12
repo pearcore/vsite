26ea2617dd461e39ee04b2977abca46137b0b65f
pearcore <pearcore@sohu.com>
2019-09-12 17:32:25 +0800

    zz

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+
+import datetime
+
 # Create your models here.
 class Question(models.Model):
     class Meta:
@@ -10,6 +13,13 @@ class Question(models.Model):
     pub_date = models.DateTimeField('发表时间')
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
