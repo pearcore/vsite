1c3dd3d1f1987f90876f6354b1a520bb1611962e
pearcore <pearcore@sohu.com>
2019-09-24 16:32:30 +0800

    zzz

--- vsite/settings.py
+++ vsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h7o#+&27w74^mzigci^@89jc62m_1o05c&if2lft)jbj*aa@7%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
